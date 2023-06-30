c6bf261db630770bf222ae101937241b9fe992e8
masi <masi@hotmail.com>
2023-06-30 19:48:27 -0300

    commit standar

--- index.php
+++ index.php
@@ -2,18 +2,19 @@
 echo "hola";
 
 class QueryMethod{
-    public $connect, $table, $operation, $query;
-  public function __construct($connect, $table, $operation, $values=[], $identifier="")
+  private $connect, $table, $values, $columns, $identifiers;
+  public function __construct($connect, $table, $operation, $values=[], $identifiers=[], $columns=[])
   {
   $this->connect = $connect;
   $this->table = $table;
-  $this->values = $values;
-  $this->condition = $condition;
+  $this->values = $values; 
+  $this->columns = $columns; 
+  //values y columns siempre seran listas, sin duda
+  $this->identifiers = $identifiers;
 
-  //CRUD = C:0, R:1, U:2, D:3
+  //CRUD = C:0, R:1 (varia si hay o no identifier), U:2, D:3
   switch ($operation) {
     case 0:
-        create_function();
         break;
     case 1:
 
@@ -30,6 +31,7 @@ class QueryMethod{
 
   }
 
+  //ej: QueryCreator($mySqli, $table, )->insert__function();
   private function insert_function(){
     
     $string = '';
@@ -42,44 +44,80 @@ class QueryMethod{
     
     $string = rtrim($string, ', ');
 
-    if ($result = $mysqli -> query("INSERT INTO $this->table VALUES ($string)")) {
-        return $result -> fetch_all();
+    if ($result = $this->connect -> query("INSERT INTO $this->table VALUES ($string)")) {
+      $response  = $result;
+      $this->connect->close();  
+      return $response;
 
     }
   }
 
 
   private function read_function(){
+  
+    if ($this->identifiers){
+      $condition='';
+      for ($i=0;$i<count($this->identifiers); $i++){
+      $condition.= "'$this->identifiers[$i]'='$this->values[$i], '";
+  }
+  $condition = rtrim($condition, ', ');
 
-    if ($result = $mysqli -> query("SELECT * FROM $this->table")) {
-        return $result -> fetch_all();
+      if ($result = $this->connect -> query("SELECT * FROM $this->table WHERE $condition")) {
+        $response  = $result -> fetch_all();
+        $this->connect->close();  
+        return $response;
 
     }
+    }
+    else{
+    if ($result = $this->connect -> query("SELECT * FROM $this->table")) {
+      $response  = $result -> fetch_all();
+      $this->connect->close();  
+      return $response;
+
+  }
+    }
+
   }
 
 
-  private function update_function(){
-
+  private function update_function($pkeys){
+    // Esta es medio un embole ya que necesita los valores a cambiar, las casillas a cambiar, el identificador
+    //que sera una cantidad de casillas pk, y el valor que han de tener, el cual es parte de columnas y valores
     $string = '';
-    for($i=0; $i<count($columns);$i++){
-        $string .= "'$value =$this->values[$i]'='$column=$this->columns[$i]', ";
+    for($i=0; $i<count($this->columns);$i++){
+        $string .= "'$this->values[$i]'='$this->columns[$i]', ";
 
     }
 
     $string = rtrim($string, ', ');
-    $identifier= "'$columns[0]'='$values[0]'";
-        if ($result = $mysqli -> query("UPDATE $this->table SET $string WHERE $string")) {
-        return $result -> fetch_all();
+    
+    $condition ='';
+    for ($i=0;$i<count($this->identifiers); $i++){
+    $condition.= "'$this->identifiers[$i]'='$this->values[$i], '";
+}
+$condition = rtrim($condition, ', ');
+        if ($result = $this->connect -> query("UPDATE $this->table SET $string WHERE $condition")) {
+          $this->connect->close();  
+          $response  = $result;
+          $this->connect->close();  
+          return $response;
     }
 
   }
   private function delete_function(){
-    if ($result = $mysqli -> query("SELECT * FROM $this->table")) {
-        return $result -> fetch_all();
+    $condition ='';
+    for ($i=0;$i<count($this->identifiers); $i++){
+    $condition.= "'$this->identifiers[$i]'='$this->values[$i], '";
+}
+$condition = rtrim($condition, ', ');
+    if ($result = $this->connect -> query("DELETE FROM $this->table WHERE $condition")) {
+      $this->connect->close();  
+      $response  = $result;
+      $this->connect->close();  
+      return $response;
     }
 
   }
   }
-
-
 ?>
