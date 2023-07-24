c7f48ada2b51a8af5e9c50f835eabac278ff0f57
Hexdy <maximilianomarquez12@hotmail.com>
2023-07-24 19:03:45 -0300

    commit standard

--- server.php
+++ server.php
@@ -10,13 +10,14 @@ header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 header("Access-Control-Allow-Credentials: true");
 
-
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if (isset($_POST['functionName'])) {
         $functionName = $_POST['functionName'];
 
+        unset($_POST['functionName']);
+
         if (function_exists($functionName)) {
-            $result = call_user_func_array($functionName, $_POST);
+            $result = call_user_func_array($functionName, array_values($_POST));
 
             echo json_encode($result);
         } else {
